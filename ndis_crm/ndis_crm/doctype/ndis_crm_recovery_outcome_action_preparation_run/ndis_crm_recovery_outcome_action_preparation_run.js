5c20a12914248c7a9ffeb5d242217379c8bfb5ee
rapidlodge <rapid.lodge@gmail.com>
2026-08-12 11:46:51 +0000

    Add NDIS CRM controlled recovery outcome action preparation bridge

---
+++ ndis_crm/ndis_crm/doctype/ndis_crm_recovery_outcome_action_preparation_run/ndis_crm_recovery_outcome_action_preparation_run.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Hex Flow and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("NDIS CRM Recovery Outcome Action Preparation Run", {
+// 	refresh(frm) {
+
+// 	},
+// });
